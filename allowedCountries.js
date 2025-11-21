26ec9937ea3fdfc1fc589ade9f604b81e5b6e1e3
tkxd369-png <tkxd369@gmail.com>
2025-11-21 11:17:52 -0800

    Create allowedCountries.js

---
+++ allowedCountries.js
@@ -0,0 +1,10 @@
+// Países permitidos para payouts (Stripe Connect)
+export const ALLOWED_COUNTRIES = [
+  "US", // Estados Unidos
+  "MX", // México
+  "BR", // Brasil
+  "CL", // Chile
+  "CO", // Colombia
+  "PE", // Perú
+  "CA", // Canadá
+];
