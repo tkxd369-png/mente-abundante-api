63cd04720f25c7d8966ac3e1b4ae025d19d9083c
tkxd369-png <244621572+tkxd369-png@users.noreply.github.com>
2026-09-14 15:45:46 -0700

    Update allowedCountries.js

--- allowedCountries.js
+++ allowedCountries.js
@@ -1,12 +1,6 @@
  // allowedCountries.js
 const ALLOWED_COUNTRIES = [
   "US", // Estados Unidos
-  "MX", // México
-  "BR", // Brasil
-  "CL", // Chile
-  "CO", // Colombia
-  "PE", // Perú
-  "CA", // Canadá
 ];
 
 module.exports = { ALLOWED_COUNTRIES };
